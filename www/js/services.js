fb335341cb05cae26f2fd742300867bb39eeb70f
Joseph Won <godspeedyoo@gmail.com>
2015-02-17 13:33:16 -0800

    Remove template factory for Messages and chats

--- www/js/services.js
+++ www/js/services.js
@@ -1,26 +1,5 @@
 angular.module('starter.services', [])
 
-.factory('Messages', function() {
-  // Might use a resource here that returns a JSON array
-
-  return {
-    all: function() {
-      return chats;
-    },
-    remove: function(chat) {
-      chats.splice(chats.indexOf(chat), 1);
-    },
-    get: function(chatId) {
-      for (var i = 0; i < chats.length; i++) {
-        if (chats[i].id === parseInt(chatId)) {
-          return chats[i];
-        }
-      }
-      return null;
-    }
-  }
-})
-
 .factory('Auth', function($auth) {
   var user = null
   return {
